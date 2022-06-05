ba5e4740c3732eadf570effc248d0ea9c5ebb505
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-05 06:24:02 +0545

    first commit

--- arabfonts/settings.py
+++ arabfonts/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL  = '/media/'
 
 # Default primary key field type
